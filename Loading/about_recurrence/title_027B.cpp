e58760253555aff3a3ab47551004bc0403ebe7eb
Syoukawa <chernoar_syoukawa_bisyoufish@outlook.com>
2017-04-26 18:23:25 +0800

    修正无法跳出的问题。

---
+++ Loading/about_recurrence/title_027B.cpp
@@ -0,0 +1,20 @@
+#include"usual_public_functions.h"
+#include"titles_check_functions.h"
+//#include <string>
+#define TEMP_SIZE 100
+using namespace std;
+
+void title_27B() {
+	int n(0);
+	printf("Please input the num of string:");
+	cin >> n;
+	printf("\n");
+	printf("Please input the string needed reverse:");
+	//char c;
+	//debug using/ cin >> temp_string;
+
+	//while ((c = getchar()) != '\n') temp_string[i++] = c;
+	//debug using/ output_end2begin(temp_string);
+
+	reverse(n);
+}
---
+++ Loading/title_027.cpp
@@ -0,0 +1,36 @@
+#include"usual_public_functions.h"
+#include"titles_check_functions.h"
+//#include <string>
+#define TEMP_SIZE 100
+using namespace std;
+
+////////需要更改的代码！//////////
+
+void reverse(int n);
+
+void title_27() {
+	printf("Please input the string needed reverse:");
+	char temp_string[TEMP_SIZE]{ '0' }; //debug using/ ="123";
+	int i(0);
+	char c;
+	//debug using/ cin >> temp_string;
+
+	while ((c=getchar()) != '\n') temp_string[i++] = c;
+	//debug using/ output_end2begin(temp_string);
+
+	reverse(sizeof(temp_string)/sizeof(char));
+}
+
+/*void reverse(int n) {
+	char temp_s;
+	if (n <= 1) {
+		temp_s = getchar();
+		printf("%c", temp_s);
+	}
+	else {
+		temp_s = getchar();
+		reverse(n - 1);
+		printf("%c", temp_s);//first.get -> final.output
+	}
+	puts("\n\n");
+}*/
