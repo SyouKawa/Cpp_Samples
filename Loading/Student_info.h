5ff418fab1799576f4f6a30679fa95d484f2e486
Syoukawa <chernoar_syoukawa_bisyoufish@outlook.com>
2017-05-08 16:36:14 +0800

    Student_info  Alpha.ver

--- Loading/Student_info.h
+++ Loading/Student_info.h
@@ -3,15 +3,33 @@
 #define __STUDENT_INFO_H_
 
 #include"titles_check_functions.h"
+#include<string>
 
-typedef struct Student_info* student_link;
+typedef class Student_info* student_link;
 typedef struct list* Position;
 
-struct Student_info {
-	std::string student_num;
+class Student_info
+{
+public:
+	Student_info(const std::string &IDcode) :ID(IDcode), score(0) {}
+	Student_info() :score(0) {}
+	Student_info(std::istream &is) { is >> *this; }
+	friend std::istream& operator >> (std::istream &, Student_info &);
+	friend std::ostream& operator << (std::ostream &, const Student_info &);
+
+	std::string ID;
 	int score;
 };
-
+inline std::istream& operator >> (std::istream &in, Student_info &s)
+{
+	in >> s.ID >> s.score;
+	return in;
+}
+inline std::ostream& operator<<(std::ostream &out,const Student_info &s)
+{
+	out << s.ID << "\t" << s.score << "\t" << std::endl;
+	return out;
+}
 struct list
 {
 	Student_info student;
